21a61e01511e77e5363650750b06704c5625c362
Michael Bayne <mdb@samskivert.com>
2018-07-06 20:48:12 -0700

    Update to new indenter world order.

--- src/scala/scaled/groovy/GroovyMode.scala
+++ src/scala/scaled/groovy/GroovyMode.scala
@@ -5,7 +5,7 @@
 package scaled.groovy
 
 import scaled._
-import scaled.code.Commenter
+import scaled.code.{Commenter, BlockIndenter}
 import scaled.grammar.GrammarCodeMode
 
 @Major(name="groovy",
@@ -17,7 +17,18 @@ class GroovyMode (env :Env) extends GrammarCodeMode(env) {
 
   override def langScope = "source.groovy"
 
-  override protected def createIndenter = new GroovyIndenter(config)
+  override protected def createIndenter = new BlockIndenter(config, Std.seq(
+    // bump extends/implements in two indentation levels
+    BlockIndenter.adjustIndentWhenMatchStart(Matcher.regexp("(extends|implements)\\b"), 2),
+    // align changed method calls under their dot
+    new BlockIndenter.AlignUnderDotRule(),
+    // handle javadoc and block comments
+    new BlockIndenter.BlockCommentRule(),
+    // handle indenting switch statements properly
+    new BlockIndenter.SwitchRule(),
+    // handle continued statements, with some special sauce for : after case
+    new BlockIndenter.CLikeContStmtRule()
+  ))
 
   override val commenter = new Commenter() {
     override def linePrefix  = "//"
